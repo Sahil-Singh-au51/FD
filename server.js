9dbffa285bd4afbf4920f115cc8a83eacf0da094
Sahil-Singh-au51 <113921049+Sahil-Singh-au51@users.noreply.github.com>
2023-05-21 15:52:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,14 +53,14 @@ app.use("/api", braintreeRoutes);
 app.use("/api", orderRoutes);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
+
   // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-}
+
 
 
 
